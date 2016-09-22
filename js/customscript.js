7c86f389bea36e53ce56a26b8578647c64d5cfb3
Rahul Dhiman <rahulmyzone@gmail.com>
2016-09-22 10:04:17 +0530

    Updated the shakti design

--- js/customscript.js
+++ js/customscript.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
   var owl = $("#owl-demo");
  
   owl.owlCarousel({
-      items : 6, //10 items above 1000px browser width
-      itemsDesktop : [1000,4], //5 items between 1000px and 901px
-      itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      itemsTablet: [600,2], //2 items between 600 and 0
+      items : 2, //10 items above 1000px browser width
+      itemsDesktopSmall : [900,1], // betweem 900px and 601px
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
+  
+  var owl = $("#homeCarousel");
+ 
+  owl.owlCarousel({
+      items : 1, //10 items above 1000px browser width
       itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
   });
  
